f3526d786093096815786287759c4b1cc401bbe5
Pranay Shah <32932413+PranayS1@users.noreply.github.com>
2022-08-25 14:48:40 +0530

    Formatting

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,27 @@
-const path = require("path")
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const postsRoutes = require('./routes/posts');
-const userRoutes = require('./routes/user');
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 
 var app = express();
 
-mongoose.connect(
-        "mongodb+srv://Pranay:Lh5H1lXeiGbGda9n" +
-        // + process.env.MONGO_ATLAS_PW +
-        "@cluster0-ik25u.mongodb.net/node-angular?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }
-    )
-    .then(() => {
-        console.log('Connected to db!');
-    })
-    .catch((err) => {
-        console.log(process.env);
-        console.log('Connection failed!');
-    })
+mongoose
+  .connect(
+    "mongodb+srv://Pranay:Lh5H1lXeiGbGda9n" +
+      // + process.env.MONGO_ATLAS_PW +
+      "@cluster0-ik25u.mongodb.net/node-angular?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Connected to db!");
+  })
+  .catch((err) => {
+    console.log(process.env);
+    console.log("Connection failed!");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,22 +31,22 @@ app.use("/", express.static(path.join(__dirname, "angular")));
 // below CORS settings are not required while deploying app as single package for both frontend and backend
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        "Original, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
-})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Original, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "angular", "index.html"));
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
 });
 
 module.exports = app;
